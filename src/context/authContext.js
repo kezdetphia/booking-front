dd028603af82de92ad2264b1d651ba213687235b
kezdetphia <fehermark88@gmail.com>
2024-10-09 19:33:40 -0700

    clean files

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -7,10 +7,7 @@ export const AuthContextProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(undefined);
   const [authLoading, setAuthLoading] = useState(true);
 
-  console.log("AuthContextProvider", user);
-
   useEffect(() => {
-    console.log("AuthContextProvider useffect", user);
     const setUserAuthenticated = async () => {
       try {
         const token = localStorage.getItem("authToken");
@@ -29,24 +26,14 @@ export const AuthContextProvider = ({ children }) => {
           }
 
           const data = await res.json();
-          console.log("home userrrr from token", data.user);
           setUser(data.user);
 
-          // const userDetails = localStorage.getItem("user");
-          // if (userDetails) {
-          //   setUser(JSON.parse(userDetails));
           setIsAuthenticated(true);
         } else {
           setUser(null);
           setIsAuthenticated(false);
-          //added after editing
           console.log("No user or authentication token found");
         }
-        // } else {
-        //   setUser(null);
-        //   setIsAuthenticated(false);
-        //   console.log("No user or authentication token found");
-        // }
       } catch (error) {
         console.error("Failed to fetch auth token", error);
         setUser(null);
@@ -58,19 +45,12 @@ export const AuthContextProvider = ({ children }) => {
     setUserAuthenticated();
   }, []);
 
-  // const setUserInfo = async (userDetails) => {
-  //   localStorage.setItem("user", JSON.stringify(userDetails));
-  //   setUser(userDetails);
-  // setIsAuthenticated(true);
-  // };
-
   return (
     <AuthContext.Provider
       value={{
         isAuthenticated,
         user,
         authLoading,
-        // setUserInfo,
         setIsAuthenticated,
         setUser,
       }}
--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -2,22 +2,6 @@ import React, { useEffect } from "react";
 import { useAuth } from "../../context/authContext";
 
 const Home = () => {
-  // useEffect(() => {
-  //   const getUserByToken = async () => {
-  //     const res = await fetch("http://localhost:3001/api/users/getmyuser", {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${localStorage.getItem("authToken")}`,
-  //       },
-  //     });
-
-  //     const data = await res.json();
-  //     console.log("home userrrr from token", data.user);
-  //   };
-  //   getUserByToken();
-  // }, []);
-
   return (
     <div className="flex justify-center items-center bg-red-200">
       <h1>home</h1>
